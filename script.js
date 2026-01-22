ee29a0bbf084c71ce00295de6c3055d9e11e5c32
Ozed-Da-Programmer <tonyrich0729@gmail.com>
2026-01-22 05:51:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const chatForm = document.getElementById("chatForm");
+
+const messageInput = document.getElementById("messageInput");
+
+const chatHistory = document.getElementById("chatHistory");
+
+chatForm.addEventListener("submit", function (event) {
+    event.preventDefault ();
+
+    const messageText = messageInput.value.trim();
+
+    if (messageText === "") {
+        return;
+    }
+
+    const messageDiv = document.createElement("div");
+    messageDiv.classList.add("message", "user-message");
+    messageDiv.textContent = messageText;
+
+    chatHistory.appendChild(messageDiv);
+
+    chatHistory.scrollTop = chatHistory.scrollHeight;
+
+    messageInput.value = ""
+})
